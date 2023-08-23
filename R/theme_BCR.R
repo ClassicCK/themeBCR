74ae968b8295a9db0fa8434a8621e4329575675c
Christopher Kilner <31750239+ClassicCK@users.noreply.github.com>
2023-08-23 17:12:28 -0400

    argh

--- R/theme_BCR.R
+++ R/theme_BCR.R
@@ -23,7 +23,7 @@
 #' @family themes
 #' @export
 #' @importFrom ggplot2 theme_grey
-theme_foundation <- function(base_size=12, base_family= c("Exo 2", "sans-serif")){
+theme_foundation <- function(base_size=12, base_family= ""){
   thm <- theme_grey(base_size = base_size, base_family = base_family)
   for (i in names(thm)) {
     if ("colour" %in% names(thm[[i]])) {
@@ -48,7 +48,7 @@ theme_foundation <- function(base_size=12, base_family= c("Exo 2", "sans-serif")
 #' @export
 #' @importFrom grid unit
 themeBCR <- function() {
-  theme_foundation(base_size = 15, base_family = c("Exo 2", "sans-serif")) +
+  theme_foundation(base_size = 15, base_family = "") +
     theme(
       plot.background = element_rect(fill = "white"),
       panel.background = element_rect(fill = "white"),
@@ -66,7 +66,7 @@ themeBCR <- function() {
       panel.grid.minor = element_blank(),
       plot.margin = unit(c(1, 1, 1, 1), "lines"),
       strip.background = element_rect(),
-      text = element_text(family = base_family, colour = "#3C3C3C"),
+      text = element_text(colour = "#3C3C3C"),
       plot.caption = element_text(size = rel(0.7), hjust = 0),
       plot.caption.position = "plot",
       plot.tag = element_text(
